4d767c791845ca1e570f51dd30e4324afcb5b0db
Valoriaz <lolcarry7@gmail.com>
2022-05-27 23:52:53 +0200

    Update base_npc_emblem_transfer.sql

--- sql/world/base_npc_emblem_transfer.sql
+++ sql/world/base_npc_emblem_transfer.sql
@@ -7,7 +7,7 @@ SET
 -- NPC
 DELETE FROM creature_template WHERE entry = @Entry;
 INSERT INTO creature_template (`entry`, `modelid1`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `scale`, `rank`, `unit_class`, `unit_flags`, `type`, `type_flags`, `RegenHealth`, `flags_extra`, `ScriptName`) VALUES
-(@Entry, @Model, @Name, @Title, "Buy", NULL, 80, 80, 35, 1, 1, 1.14286, 1, 0, 1, 2, 7, 138936390, 1, 16777216, "npc_emblem_transfer");
+(@Entry, @Model, @Name, @Title, "Buy", 0, 80, 80, 35, 1, 1, 1.14286, 1, 0, 1, 2, 7, 138936390, 1, 16777216, "npc_emblem_transfer");
 
 -- NPC Text
 DELETE FROM `npc_text` WHERE `ID`=@Entry;
